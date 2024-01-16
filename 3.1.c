424f620ad14abb9079401e50bcaf6f3df4826ade
themosgit <47158771+themosgit@users.noreply.github.com>
2024-01-16 18:55:25 +0200

    Add files via upload

---
+++ 3.1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include "timer.h"
+#include <time.h>
+
+
+long long int monte_carlo(long long int my_first_i, long long int my_last_i );
+
+int main(int argc, char *argv[]) {
+    int source;
+    long long int result_velh;
+    long long int local_result_velh;
+    long long int plhuos_ripsewn = 10000000;
+
+    MPI_Init(&argc, &argv);
+
+    int my_rank, comm_sz;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+
+    srand(time(NULL) + my_rank);
+
+    long long int my_n = plhuos_ripsewn / comm_sz;
+    long long int my_first_i = my_n * my_rank;
+    long long int my_last_i = my_first_i + my_n;
+    local_result_velh = monte_carlo(my_first_i, my_last_i);
+
+    double start, finish;
+    GET_TIME(start);
+         MPI_Reduce(&local_result_velh, &result_velh, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    if(my_rank == 0){
+        GET_TIME(finish);
+        printf("velh %lld\n", result_velh);
+        printf("Elapsed time = %e seconds\n", finish - start);
+        printf("pi: %lf\n", (double) 4 * result_velh / plhuos_ripsewn);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
+
+long long int monte_carlo(long long int my_first_i, long long int my_last_i) {
+    int velh = 0;
+
+    for (long long int i = my_first_i; i < my_last_i; i++) {
+        double x = (rand() % 2000 - 1000) / 1000.0;
+        double y = (rand() % 2000 - 1000) / 1000.0;
+        double tetragwno = x * x + y * y;
+        if (tetragwno <= 1) {
+            velh++;
+        }
+    }
+
+    return velh;
+
+}
